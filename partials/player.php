0a649301683469723ff5cdf589c1156b422c68ec
cas <cas@interglobal.vision>
2017-06-20 14:58:49 -0500

    add basic functionality: player fadesIn on load

--- partials/player.php
+++ partials/player.php
@@ -1,4 +1,3 @@
 <section class="radioplayer-container">
-<audio id="radio-player" controls><source src="http://streaming.radio.co/s0b5e9c02c/listen" type="audio/mpeg"></audio>
-
+  <audio id="radio-player"><source src="http://streaming.radio.co/s0b5e9c02c/listen" type="audio/mpeg"></audio>
 </section>
--- src/js/main.js
+++ src/js/main.js
@@ -11,7 +11,7 @@ Site = {
     });
 
     $(document).ready(function () {
-
+      Site.Player.init();
     });
 
   },
@@ -28,6 +28,63 @@ Site = {
       string = string.replace(/ ([^ ]*)$/,'&nbsp;$1');
       $(this).html(string);
     });
+  },
+};
+
+Site.Player = {
+  fadeTime:  2000,
+  statusUrl: 'https://public.radio.co/stations/s0b5e9c02c/status',
+  streamData: null,
+  init: function() {
+    var _this = this;
+
+    _this.playerElement =  document.getElementById('radio-player');
+
+    // Check if the stream is online
+    _this.checkStreamStatus();
+
+  },
+
+  // Check the stream status thru ajax and change the player accordingly
+  checkStreamStatus: function() {
+    var _this = this;
+
+    // Make the ajax request
+    $.getJSON(_this.statusUrl, function(data) {
+      _this.streamData = data.status;
+
+      // If the stream is offline
+      if(data.status == 'offline') {
+        _this.pause(); // Pause the stream
+      } else if (_this.playerElement.paused) { // If stream is online and the player is paused
+        _this.play();
+      }
+    });
+
+  },
+
+  play: function() {
+    var _this = this;
+
+    _this.playerElement.volume = 0;
+    _this.playerElement.play();
+
+    var volumeIncrement = 1 / (_this.fadeTime / 50);
+    var fadeInterval = setInterval( function() {
+      _this.playerElement.volume += volumeIncrement;
+
+      if (_this.playerElement.volume >= 1 - volumeIncrement) {
+        _this.playerElement.volume = 1
+        clearInterval(fadeInterval);
+      }
+
+    }, 50);
+  },
+
+  pause: function() {
+    var _this = this;
+
+    _this.playerElement.pause();
   },
 };
 
