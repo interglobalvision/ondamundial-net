4be734680f6ebd26e4462d6f8e9cd7e3c9f24364
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-06-20 13:15:35 -0500

    add evento post type

--- lib/post-types.php
+++ lib/post-types.php
@@ -3,49 +3,49 @@
 // https://developer.wordpress.org/resource/dashicons/
 function add_menu_icons_styles(){
 ?>
- 
+
 <style>
-#menu-posts-project .dashicons-admin-post:before {
+#menu-posts-evento .dashicons-admin-post:before {
     content: '\f319';
 }
 </style>
- 
+
 <?php
 }
 add_action( 'admin_head', 'add_menu_icons_styles' );
 
 
 //Register Custom Post Types
-add_action( 'init', 'register_cpt_project' );
+add_action( 'init', 'register_cpt_evento' );
 
-function register_cpt_project() {
+function register_cpt_evento() {
 
-    $labels = array( 
-        'name' => _x( 'Projects', 'project' ),
-        'singular_name' => _x( 'Project', 'project' ),
-        'add_new' => _x( 'Add New', 'project' ),
-        'add_new_item' => _x( 'Add New Project', 'project' ),
-        'edit_item' => _x( 'Edit Project', 'project' ),
-        'new_item' => _x( 'New Project', 'project' ),
-        'view_item' => _x( 'View Project', 'project' ),
-        'search_items' => _x( 'Search Projects', 'project' ),
-        'not_found' => _x( 'No projects found', 'project' ),
-        'not_found_in_trash' => _x( 'No projects found in Trash', 'project' ),
-        'parent_item_colon' => _x( 'Parent Project:', 'project' ),
-        'menu_name' => _x( 'Projects', 'project' ),
+    $labels = array(
+        'name' => _x( 'Eventos', 'evento' ),
+        'singular_name' => _x( 'Evento', 'evento' ),
+        'add_new' => _x( 'Añadir Nuevo', 'evento' ),
+        'add_new_item' => _x( 'Add Añadir Nuevo evento', 'evento' ),
+        'edit_item' => _x( 'Editar evento', 'evento' ),
+        'new_item' => _x( 'Nuevo evento', 'evento' ),
+        'view_item' => _x( 'Ver evento', 'evento' ),
+        'search_items' => _x( 'Buscar eventos', 'evento' ),
+        'not_found' => _x( 'No se encontraron eventos', 'evento' ),
+        'not_found_in_trash' => _x( 'Ningún evento encontrado en la papelera', 'evento' ),
+        'parent_item_colon' => _x( 'Parent evento:', 'evento' ),
+        'menu_name' => _x( 'Programación', 'evento' ),
     );
 
-    $args = array( 
+    $args = array(
         'labels' => $labels,
         'hierarchical' => false,
-        
+
         'supports' => array( 'title', 'editor', 'thumbnail' ),
-        
+
         'public' => true,
         'show_ui' => true,
         'show_in_menu' => true,
         'menu_position' => 5,
-        
+
         'show_in_nav_menus' => true,
         'publicly_queryable' => true,
         'exclude_from_search' => false,
@@ -56,5 +56,5 @@ function register_cpt_project() {
         'capability_type' => 'post'
     );
 
-    register_post_type( 'project', $args );
+    register_post_type( 'evento', $args );
 }
