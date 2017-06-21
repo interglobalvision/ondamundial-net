b002e54c253e8189c992656f964a8c1f0095cb59
cas <cas@interglobal.vision>
2017-06-20 19:45:55 -0500

    improve player event handling. add online class

--- partials/player.php
+++ partials/player.php
@@ -1,6 +1,9 @@
-<section class="radioplayer-container">
-  <audio id="radio-player" controls><source id="player-src" type="audio/mpeg"></audio>
+<section id="player-container">
+  <audio id="player" controls>
+    <source id="player-src" type="audio/mpeg" />
+  </audio>
   <p>
+    <span id="offline-icon"><a href="#programacion">Programación</a></span>
     <span id="player-control"></span>
     <span id="stream-status"></span>
     <span id="now-playing"></span>
--- src/js/main.js
+++ src/js/main.js
@@ -11,6 +11,7 @@ Site = {
     });
 
     $(document).ready(function () {
+      Site.StreamChecker.init();
       Site.Player.init();
     });
 
@@ -31,35 +32,30 @@ Site = {
   },
 };
 
-Site.Player = {
-  neverStarted: true,
-  fadeTime:  2000,
-  statusCheckInterval: 5000,
+Site.StreamChecker = {
   statusUrl: 'https://public.radio.co/stations/s0b5e9c02c/status',
-  streamUrl: 'http://streaming.radio.co/s0b5e9c02c/listen',
+  statusCheckInterval: 5000,
   streamData: null,
   init: function() {
     var _this = this;
 
-    _this.playerElement =  document.getElementById('radio-player');
-    _this.playerSrc =  document.getElementById('player-src');
-    _this.playerControl =  document.getElementById('player-control');
-    _this.streamStatusText =  document.getElementById('stream-status');
-    _this.nowPlayingText =  document.getElementById('now-playing');
+    _this.startChecker();
+  },
 
-    // Check if the stream is online
-    _this.checkStreamStatus(true);
+  startChecker: function() {
+    var _this = this;
+
+
+    _this.checkStreamStatus();
 
     // Start checker on an interval
     setInterval(function() {
       _this.checkStreamStatus();
     }, _this.statusCheckInterval);
-
-
   },
 
   // Check the stream status thru ajax and change the player accordingly
-  checkStreamStatus: function(forcePlay) {
+  checkStreamStatus: function() {
     var _this = this;
 
     // Make the ajax request
@@ -70,43 +66,105 @@ Site.Player = {
 
       // Check if the stream is offline
       if(data.status == 'offline') {
-        // Update marquee status
-        _this.streamStatusText.innerHTML = 'Siguiente: ';
 
-        // Update the marquee text
-        _this.nowPlayingText.innerHTML = 'the upcoming show that has to be requested thru ajax';
+        _this.triggerEvent('streamoffline', data);
 
-        // Pause the stream
-        _this.pause();
+      } else if(data.status === 'online')  {  // else  (stream is online)
 
-      } else {  // else  (stream is online)
-
-        // Check if player src is empty
-        if (_this.playerSrc.src === '') {
-          // Add src
-          _this.playerSrc.src =  _this.streamUrl;
-
-          // Load src
-          _this.playerElement.load();
-        }
-
-        // Update marquee status
-        _this.streamStatusText.innerHTML = 'Ahora: ';
-
-        // Update Now playing
-        _this.nowPlayingText.innerHTML = data.current_track.title;
-
-        if (_this.playerElement.paused && (forcePlay === true || _this.neverStarted) ) {
-          var canplayListener = _this.playerElement.addEventListener('canplay', function() {
-            // If the player is paused and [forcePlay is true  or the player has never started playing]
-            _this.play();
-            _this.playerElement.removeEventListener('canplay', canplayListener);
-          });
-        }
-
+        _this.triggerEvent('streamonline', data);
       }
     });
 
+  },
+
+  triggerEvent: function(eventName, detail) {
+    var _this =  this;
+
+    console.log('event triggered: ', eventName);
+
+    var customEvent = new CustomEvent(eventName, {
+      detail: detail,
+    });
+
+    document.dispatchEvent(customEvent);
+  },
+
+};
+
+Site.Player = {
+  neverPlayed: true,
+  fadeTime:  2000,
+  streamUrl: 'http://streaming.radio.co/s0b5e9c02c/listen',
+  init: function() {
+    var _this = this;
+
+    // Player DOM elements
+    _this.playerContainer =  document.getElementById('player-container');
+    _this.playerElement =  document.getElementById('player');
+    _this.playerSrc =  document.getElementById('player-src');
+    _this.playerControl =  document.getElementById('player-control');
+    _this.streamStatusText =  document.getElementById('stream-status');
+    _this.nowPlayingText =  document.getElementById('now-playing');
+
+    // Bind event handlers
+    _this.handleOnlineStream = _this.handleOnlineStream.bind(_this);
+    _this.handleCanPlay = _this.handleCanPlay.bind(_this);
+    _this.handleOfflineStream = _this.handleOfflineStream.bind(_this);
+
+    // Subscribe to stream events
+    document.addEventListener('streamonline', _this.handleOnlineStream);
+    document.addEventListener('streamoffline', _this.handleOfflineStream);
+
+  },
+
+  handleOnlineStream: function(event) {
+    var _this = this;
+
+    var data = event.detail;
+
+    _this.playerContainer.classList.add('online');
+
+    // Check if player src is empty
+    if (_this.playerSrc.src === '' || _this.playerElement.paused) {
+      // Add src
+      _this.playerSrc.src =  _this.streamUrl;
+
+      // Load src
+      _this.playerElement.load();
+    }
+
+    // Update marquee status
+    _this.streamStatusText.innerHTML = 'Ahora: ';
+
+    // Update Now playing
+    _this.nowPlayingText.innerHTML = data.current_track.title;
+
+    if (_this.neverPlayed) {
+      _this.playerElement.addEventListener('canplay', _this.handleCanPlay);
+    }
+  },
+
+  handleCanPlay: function(event) {
+    var _this = this;
+
+    _this.play();
+    event.target.removeEventListener(event.type, _this.handleCanPlay);
+
+  },
+
+  handleOfflineStream: function() {
+    var _this = this;
+
+    _this.playerContainer.classList.remove('online');
+
+    // Update marquee status
+    _this.streamStatusText.innerHTML = 'Siguiente: ';
+
+    // Update the marquee text
+    _this.nowPlayingText.innerHTML = 'the upcoming show that has to be requested thru ajax';
+
+    // Pause the stream
+    _this.pause();
   },
 
   play: function() {
@@ -117,14 +175,14 @@ Site.Player = {
 
       _this.playerElement.play();
 
-      _this.neverStarted = false;
+      _this.neverPlayed = false;
 
       var volumeIncrement = 1 / (_this.fadeTime / 50);
       var fadeInterval = setInterval( function() {
         _this.playerElement.volume += volumeIncrement;
 
         if (_this.playerElement.volume >= 1 - volumeIncrement) {
-          _this.playerElement.volume = 1
+          _this.playerElement.volume = 1;
           clearInterval(fadeInterval);
         }
       }, 50);
