6fafe3d676778b73fa546295e4c2f760594f5e14
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-06-21 14:27:40 -0500

    rewrite geteventsobject func for passing json array to js

--- lib/functions-custom.php
+++ lib/functions-custom.php
@@ -2,8 +2,10 @@
 
 // Custom functions (like special queries, etc)
 
-function get_events_array( $query_args = '' ) {
-  $args = wp_parse_args( $query_args, array(
+// Returns object of events to JS as WP.eventsObject
+function get_events_object() {
+  // Events since 24 hours ago
+  $args = array(
     'post_type' => 'evento',
     'meta_key' => '_igv_evento_start',
     'order' => 'ASC',
@@ -11,29 +13,27 @@ function get_events_array( $query_args = '' ) {
     'meta_query' => array(
       array(
         'key'     => '_igv_evento_start',
-        'value'   => current_time('timestamp') - (60 * 60 * 24), // UTC -5, 6 hours ago
+        'value'   => time() - (60 * 60 * 24), // UTC -5, 24 hours ago
         'compare' => '>',
       )
     )
-  ) );
+  );
+
   $posts = get_posts( $args );
-  $event_array = array();
+  $events_object = array();
 
   if ( $posts ) {
     foreach ( $posts as $post ) {
       $timestamp = get_post_meta($post->ID, '_igv_evento_start', true);
-      $year = date('Y', $timestamp);
-      $month = date('F', $timestamp);
-      $day = date('l j', $timestamp);
 
-      $event_array[$year][$month][$day][] = array(
+      $events_object[] = array(
         'timestamp' => $timestamp,
         'title' => $post->post_title,
-        'content' => $post->post_content
+        'content' => apply_filters('the_content', $post->post_content),
       );
     }
-    return $event_array;
+    return json_encode($events_object);
   } else {
-    return array();
+    return null;
   }
 }
