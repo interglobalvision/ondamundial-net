8bdf6866b21d76d6fb2583d2bba04ad760d2e651
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-06-21 20:15:35 -0500

    change events array localized js key name

--- functions.php
+++ functions.php
@@ -19,7 +19,7 @@ function scripts_and_styles_method() {
     'siteUrl' => home_url(),
     'themeUrl' => get_template_directory_uri(),
     'isAdmin' => $is_admin,
-    'eventsObject' => get_events_object(),
+    'programacionEvents' => get_events_object(),
   );
 
   wp_enqueue_script('javascript-library', $javascriptLibrary, '', '', true);
