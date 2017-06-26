645d5bcc1409ba9e6f88bb7ad279dff255fd2ff6
cas <cas@interglobal.vision>
2017-06-26 18:50:27 -0500

    add image sizes

--- lib/thumbnail-sizes.php
+++ lib/thumbnail-sizes.php
@@ -9,4 +9,10 @@ if( function_exists( 'add_image_size' ) ) {
   add_image_size( 'opengraph', 1200, 630, true );
 
   add_image_size( 'gallery', 1200, 9999, false );
+
+  add_image_size( 'gallery', 1200, 9999, false );
+  add_image_size( '1680w', 1680, 9999, false ); // macbookpro
+  add_image_size( '1024w', 1024, 9999, false ); // max-widht of ipad
+  add_image_size( '736w', 736, 9999, false ); // max-width of iphone 6 plus
+  add_image_size( '568w', 568, 9999, false ); // max-width of iphone 5
 }
