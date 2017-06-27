d54f8048dcb2083919118e87dc6dfb297892f830
cas <cas@interglobal.vision>
2017-06-26 19:16:12 -0500

    add return image sizes in api

--- lib/functions-api.php
+++ lib/functions-api.php
@@ -3,6 +3,14 @@
 function current_event_call($data) {
   $current_event_id = IGV_get_option('_igv_site_options', '_igv_radio_evento_current');
 
+  $thumbnail_sizes = array(
+    '568w', // max-width of iphone 5
+    '736w', // max-width of iphone 6 plus
+    '1024w', // max-widht of ipad
+    '1680w', // macbookpro
+    'original', // original
+  );
+
   // Check if no event is selected
   if (empty($current_event_id)) {
     return false;
@@ -11,13 +19,32 @@ function current_event_call($data) {
   // Get selected event data
   $current_event = get_post($current_event_id);
 
-  // Get event's feat thumbnail
-  $event_thumbnail = get_the_post_thumbnail_url($current_event_id);
+  // Get event's thumbnail id
+  $thumbnail_id = get_post_thumbnail_id($current_event_id);
+
+  // Init empty array to store the thumbnails and it's sizes
+  $featured_thumbnail_sizes = array();
+
+  foreach($thumbnail_sizes as $size) {
+
+    // Get event's thumbnail url
+    $thumbnail = wp_get_attachment_image_src($thumbnail_id, $size);
+
+    if (!empty($thumbnail)) {
+
+      array_push($featured_thumbnail_sizes, array(
+        'url' => $thumbnail[0],
+        'width' => $thumbnail[1],
+        'height' => $thumbnail[2],
+      ));
+    }
+
+  }
 
   // Make the response array
   $response = array(
     'title' => $current_event->post_title,
-    'featured_image' => $event_thumbnail,
+    'featured_thumbnails' => $featured_thumbnail_sizes,
   );
 
   return $response;
--- lib/thumbnail-sizes.php
+++ lib/thumbnail-sizes.php
@@ -10,9 +10,8 @@ if( function_exists( 'add_image_size' ) ) {
 
   add_image_size( 'gallery', 1200, 9999, false );
 
-  add_image_size( 'gallery', 1200, 9999, false );
-  add_image_size( '1680w', 1680, 9999, false ); // macbookpro
-  add_image_size( '1024w', 1024, 9999, false ); // max-widht of ipad
-  add_image_size( '736w', 736, 9999, false ); // max-width of iphone 6 plus
   add_image_size( '568w', 568, 9999, false ); // max-width of iphone 5
+  add_image_size( '736w', 736, 9999, false ); // max-width of iphone 6 plus
+  add_image_size( '1024w', 1024, 9999, false ); // max-widht of ipad
+  add_image_size( '1680w', 1680, 9999, false ); // macbookpro
 }
