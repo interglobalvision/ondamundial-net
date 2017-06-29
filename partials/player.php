2aa219356e0104a902776d263f1e2c2554643c81
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-06-29 16:42:48 -0500

    add pointer class to player icons

--- partials/player.php
+++ partials/player.php
@@ -5,10 +5,10 @@
   <div class="container">
     <div id="player-row" class="grid-row align-items-center">
       <div id="player-control-holder" class="grid-item">
-        <span id="offline-icon"><a href="#" class="js-page-open" data-page="programacion"><?php echo url_get_contents(get_template_directory_uri() . '/dist/img/player-offline.svg'); ?></a></span>
+        <span id="offline-icon"><a href="#" class="js-page-open u-pointer" data-page="programacion"><?php echo url_get_contents(get_template_directory_uri() . '/dist/img/player-offline.svg'); ?></a></span>
         <span id="player-control">
-          <span id="play-button"><?php echo url_get_contents(get_template_directory_uri() . '/dist/img/player-play.svg'); ?></span>
-          <span id="pause-button"><?php echo url_get_contents(get_template_directory_uri() . '/dist/img/player-pause.svg'); ?></span>
+          <span id="play-button" class="u-pointer"><?php echo url_get_contents(get_template_directory_uri() . '/dist/img/player-play.svg'); ?></span>
+          <span id="pause-button" class="u-pointer"><?php echo url_get_contents(get_template_directory_uri() . '/dist/img/player-pause.svg'); ?></span>
         </span>
       </div>
       <div id="player-status" class="grid-item grid-row flex-grow">
