ce63742fea3b771e622ecea6951abafaf2540f6b
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-06-29 17:48:07 -0500

    style sobre page

--- front-page.php
+++ front-page.php
@@ -8,15 +8,24 @@ get_header();
 
   <section id="pages">
 
-    <section id="page-programacion" class="page-overlay"></section>
+    <section id="page-programacion" class="page-overlay">
+      <div class="container">
+      </div>
+    </section>
 
 <?php
   $page_sobre = get_page_by_path('sobre');
 
   if (!empty($page_sobre)) {
 ?>
-    <section id="page-sobre" class="page-overlay">
-      <?php echo apply_filters('the_content', $page_sobre->post_content); ?>
+    <section id="page-sobre" class="page-overlay padding-bottom-large">
+      <div class="container">
+        <div class="grid-row">
+          <div class="grid-item item-s-12 item-m-10 offset-m-1 font-size-mid">
+            <?php echo apply_filters('the_content', $page_sobre->post_content); ?>
+          </div>
+        </div>
+      </div>
     </section>
 <?php
   }
